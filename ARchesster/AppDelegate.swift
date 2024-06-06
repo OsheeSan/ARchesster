93099eb79280bd8474305f59c5b16ae23cf403fa
Anton Babko <anton.babko@ukma.edu.ua>
2024-06-06 13:52:02 +0300

    Initial Commit

---
+++ ARchesster/AppDelegate.swift
@@ -0,0 +1,90 @@
+//
+//  AppDelegate.swift
+//  ARchesster
+//
+//  Created by admin on 06.06.2024.
+//
+
+import UIKit
+import ARKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device. For apps that require ARKit
+                for core functionality, use the `arkit` key in the key in the
+                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+                the app from installing. (If the app can't be installed, this error
+                can't be triggered in a production scenario.)
+                In apps where AR is an additive feature, use `isSupported` to
+                determine whether to show UI for launching AR experiences.
+            """) // For details, see https://developer.apple.com/documentation/arkit
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+
+}
+
+
+enum AppError: Error {
+    case captureSessionSetup(reason: String)
+    case visionError(error: Error)
+    case otherError(error: Error)
+    
+    static func display(_ error: Error, inViewController viewController: UIViewController) {
+        if let appError = error as? AppError {
+            appError.displayInViewController(viewController)
+        } else {
+            AppError.otherError(error: error).displayInViewController(viewController)
+        }
+    }
+    
+    func displayInViewController(_ viewController: UIViewController) {
+        let title: String?
+        let message: String?
+        switch self {
+        case .captureSessionSetup(let reason):
+            title = "AVSession Setup Error"
+            message = reason
+        case .visionError(let error):
+            title = "Vision Error"
+            message = error.localizedDescription
+        case .otherError(let error):
+            title = "Error"
+            message = error.localizedDescription
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
+
+
---
+++ ARchesster/ViewController.swift
@@ -0,0 +1,197 @@
+//
+//  ViewController.swift
+//  ARchesster
+//
+//  Created by admin on 06.06.2024.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+import Vision
+import AVFoundation
+
+let USE_DEPTH = false
+let TIME_OUT = 100
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+
+    var sceneView: ARSCNView!
+    
+    let viewBackgroundColor: UIColor = UIColor.black
+    
+    var configuration: ARWorldTrackingConfiguration?
+    
+    // Hand Detection
+    var currentBuffer: CVPixelBuffer?
+    var handPoseRequest = VNDetectHumanHandPoseRequest()
+    let visionQueue = DispatchQueue(label: "handVisionQueue")
+    
+    var lefthandnode: HandNode?
+    var righthandnode: HandNode?
+    
+    var timeoutleft = TIME_OUT
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        sceneView  = ARSCNView(frame: view.frame)
+        view.addSubview(sceneView)
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.showsStatistics = false
+        
+        self.view.backgroundColor = viewBackgroundColor
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        configuration = ARWorldTrackingConfiguration()
+        
+        configuration!.planeDetection = .horizontal
+        
+        if USE_DEPTH && ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) {
+            configuration?.frameSemantics.insert(.personSegmentationWithDepth)
+        } else {
+            print("No people occlusion supported.")
+        }
+        
+        configuration?.isCollaborationEnabled = true
+        
+        configuration?.environmentTexturing = .automatic
+        
+        sceneView.session.run(configuration!)
+        
+        handPoseRequest.maximumHandCount = 1
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        guard currentBuffer == nil, case .normal = frame.camera.trackingState else {
+            return
+        }
+        
+        self.currentBuffer = frame.capturedImage
+        classifyHand()
+    }
+    
+    func classifyHand() {
+        var templefthandobs: VNHumanHandPoseObservation?
+        let handler = VNImageRequestHandler(cvPixelBuffer: currentBuffer!, orientation: .left)
+        visionQueue.async {
+            do {
+                defer {
+                    self.currentBuffer = nil
+                    
+                    self.processPoints(newhandobs: templefthandobs, handnode: &self.lefthandnode, timeout: &self.timeoutleft, color: UIColor.white)
+                }
+                
+                try handler.perform([self.handPoseRequest])
+                guard let obs1 = self.handPoseRequest.results?.first else {
+                    return
+                }
+                
+                if obs1.chirality == .left {
+                    templefthandobs = obs1
+                    if self.isOKGesture(handObservation: templefthandobs) {
+                        print("ok")
+                    } else {
+                        print("Not Ok")
+                    }
+                }
+                
+                if self.handPoseRequest.results?.count == 1 {
+                    return
+                }
+                
+            } catch {
+                print("Error: Vision request failed with error \"\(error)\"")
+            }
+        }
+        
+    }
+    
+    func isOKGesture(handObservation: VNHumanHandPoseObservation?) -> Bool {
+        guard let handObservation = handObservation else {
+            print("Error hand")
+            return false
+        }
+            
+            do {
+                let thumbTip = try handObservation.recognizedPoint(.thumbTip)
+                let indexTip = try handObservation.recognizedPoint(.indexTip)
+                
+                let distance = hypot(thumbTip.location.x - indexTip.location.x, thumbTip.location.y - indexTip.location.y)
+                
+                return distance < 0.1
+            } catch {
+                print("Error pos")
+                return false
+            }
+        }
+    
+    func processPoints(newhandobs: VNHumanHandPoseObservation?, handnode: inout HandNode?, timeout: inout Int, color: UIColor) {
+        if newhandobs != nil {
+            if handnode == nil {
+                handnode = HandNode(color: color)
+            }
+            
+            if handnode!.parent == nil {
+                self.sceneView.scene.rootNode.addChildNode(handnode!)
+            }
+            let (newconst, _) = handnode!.createConstraints(pose: newhandobs!, camera: sceneView.session.currentFrame!.camera)
+            handnode!.addConstraint(peerID: "", constraints: newconst)
+            handnode!.applyConstraints()
+            
+            timeout = TIME_OUT
+        } else if handnode != nil {
+            if timeout > 0 {
+                timeout -= 1
+            } else {
+                handnode!.removeFromParentNode()
+            }
+        }
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+                alertController.dismiss(animated: true, completion: nil)
+                self.resetTracking()
+            }
+            alertController.addAction(restartAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func resetTracking() {
+        guard let configuration = sceneView.session.configuration else { print("A configuration is required"); return }
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    private func removeAllAnchorsOriginatingFromARSessionWithID(_ identifier: String) {
+        guard let frame = sceneView.session.currentFrame else { return }
+        for anchor in frame.anchors {
+            guard let anchorSessionID = anchor.sessionIdentifier else { continue }
+            if anchorSessionID.uuidString == identifier {
+                sceneView.session.remove(anchor: anchor)
+            }
+        }
+    }
+}
+
