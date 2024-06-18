0bf3f71abe6f4bf339a92b9789e7e5e7bc61343a
OsheeSan <osheee.san@gmail.com>
2024-06-18 12:07:38 +0300

    MainMenuVC added

---
+++ ARchesster/Controllers/MainMenuViewController.swift
@@ -0,0 +1,98 @@
+//
+//  MainMenuViewController.swift
+//  ARchesster
+//
+//  Created by admin on 18.06.2024.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let playButton: UIButton =  {
+        let button = UIButton(type: .custom)
+        button.setTitle("Play", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(name: "Avenir-Black", size: 30)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        return button
+    }()
+    
+    let contactButton: UIButton =  {
+        let button = UIButton(type: .custom)
+        button.setTitle("Show on GitHub", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(name: "Avenir-Black", size: 30)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMenu()
+        view.backgroundColor = .black
+    }
+    
+    func setupMenu() {
+        view.addSubview(logoImageView)
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            logoImageView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
+            logoImageView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -20),
+            logoImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+        view.addSubview(playButton)
+        NSLayoutConstraint.activate([
+            playButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -10),
+            playButton.leftAnchor.constraint(equalTo: logoImageView.rightAnchor, constant: 20),
+            playButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
+            playButton.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3)
+        ])
+        
+        playButton.addAction(UIAction() {
+            _ in
+            let vc = ViewController()
+            vc.modalTransitionStyle = .crossDissolve
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+        }, for: .touchUpInside)
+        
+        view.addSubview(contactButton)
+        NSLayoutConstraint.activate([
+            contactButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 10),
+            contactButton.leftAnchor.constraint(equalTo: logoImageView.rightAnchor, constant: 20),
+            contactButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
+            contactButton.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3)
+        ])
+        
+        contactButton.addAction(UIAction() {
+            _ in
+            if let url = URL(string: "https://github.com/OsheeSan/ARchesster") {
+                        if UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                        }
+                    }
+        }, for: .touchUpInside)
+        
+    }
+    
+}
